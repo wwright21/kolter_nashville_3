40dc61658024a55cca1a4317dfc7ca9b5437d19a
arc-research-analytics <wwright@atlantaregional.org>
2024-08-29 08:55:30 -0400

    test update

--- kolter_streamlit.py
+++ kolter_streamlit.py
@@ -38,6 +38,7 @@ st.sidebar.markdown(
     f"<p style='text-align:center;color:#000000;'>Select metro {base_metro} county:</p>",
     unsafe_allow_html=True
 )
+st.sidebar.write("this is a test.")
 
 # create the sidebar dropdown menu
 county_list = {
